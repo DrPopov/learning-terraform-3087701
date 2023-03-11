b362fdc5590709ea6f257b4c0366f991fd5adba6
Popov Yury <44790359+DrPopov@users.noreply.github.com>
2023-03-11 15:51:39 +0100

    Change file

--- main.tf
+++ main.tf
@@ -31,40 +31,25 @@ module "blog_vpc" {
   }
 }
 
-#resource "aws_instance" "blog" {
-#  ami                    = data.aws_ami.app_ami.id
-#  instance_type          = var.instance_type
-#  subnet_id              = module.blog_vpc.public_subnets[0]
-#  vpc_security_group_ids = [module.blog_sg.security_group_id]
-#
-#  tags = {
-#    Name = "Learning Terraform"
-#  }
-#}
 
-
-module "autoscaling" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.9.0"
-  # insert the 1 required variable here
+  version = "6.5.2"
 
   name = "blog"
-  min_size = 1
-  max_size = 2 
 
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
-
-  image_id      = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
-
-
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
+  version = "~> 6.0"
 
   name = "blog-alb"
 
@@ -80,7 +65,6 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-
     }
   ]
 
@@ -93,7 +77,7 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = "Dev"
+    Environment = "dev"
   }
 }
 
