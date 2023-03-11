43c117434a16e64e143cca84a522487ba360ab1f
Popov Yury <44790359+DrPopov@users.noreply.github.com>
2023-03-11 15:20:35 +0100

    Create dev vpc

--- main.tf
+++ main.tf
@@ -14,11 +14,41 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "eu-west-1b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+  
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
+  
+  vpc_security_group = [module.blog_sg.security_gtoup_id]
+    
   tags = {
     Name = "HelloWorld"
   }
 }
+
+  module "blog_sg" {
+    source = "terraform-aws-modules/security-group/aws"
+    version = "4.13.0"
+    
+    name   = "blog"
+    vpc_id = module.vpc.public_subnets[0]
+      
+    ingress_rules = ["https-80-tcp", "https-443-tcp"]
+    ingress_cidr_blocks = ["0.0.0.0/0"]
+    egress_rules = ["all-all"]
+    egress_cidr_blocks = ["0.0.0.0/0"]
+  }
